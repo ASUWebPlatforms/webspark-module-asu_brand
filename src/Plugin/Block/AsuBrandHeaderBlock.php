7dab59701a7a194415d9fb845c58e660a4c63776
Bryan R <aubjr@asu.edu>
2023-04-03 14:53:24 -0700

    WS2-1563 - Pulled search host logic out into a service that other modules can use; Added URL encoding function to encode search terms and their non-standard characters

--- src/Plugin/Block/AsuBrandHeaderBlock.php
+++ src/Plugin/Block/AsuBrandHeaderBlock.php
@@ -113,13 +113,10 @@ class AsuBrandHeaderBlock extends BlockBase {
       'brandLink' => 'https://www.asu.edu',
     ];
     // Search settings.
-    $search_config = \Drupal::config('asu_brand.settings');
-    $props['searchUrl'] = $search_config->get('asu_brand.asu_brand_search_url');
-    $local_search_url = $search_config->get('asu_brand.asu_brand_local_search_url');
-    $props['site'] = $local_search_url ?? \Drupal::request()->getHost();
-    // "opt-out" for use by ASU CMS, Integrated Search or with special
-    // dispensation.
-    if ($props['site'] == "opt-out") {
+    (array)$urls = \Drupal::service('asu_brand.helper_functions')->getSearchHosts();
+    $props['searchUrl'] = $urls['asu_search_url'];
+    $props['site'] = $urls['url_host'];
+    if ($props['site'] === '') { // "opt-out" was selected
       unset($props['site']);
     }
 
